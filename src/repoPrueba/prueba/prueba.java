da37b5f819454e94f7f3ba65adf595ffb13740fc
DESKTOP-O72PH1Q\Esti <estibalizmares@gmail.com>
2022-05-02 20:09:22 +0200

    Debería dar error a la hora de hacer push

---
+++ src/repoPrueba/prueba/prueba.java
@@ -0,0 +1,13 @@
+package repoPrueba.prueba;
+
+public class prueba {
+
+	public static void main(String[] args) {
+
+		System.out.println("Una pruebica pa no liarnos");
+		
+		System.out.println("ESTO DEBERÍA DAR ERROR");
+		
+	}
+
+}
