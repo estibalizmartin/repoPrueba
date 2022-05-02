7f7b1e5a873928c261a312b6aa399c05a1d25233
DESKTOP-O72PH1Q\Esti <estibalizmares@gmail.com>
2022-05-02 20:10:18 +0200

    No ha dado error, a ver esto

--- src/repoPrueba/prueba/prueba.java
+++ src/repoPrueba/prueba/prueba.java
@@ -3,10 +3,8 @@ package repoPrueba.prueba;
 public class prueba {
 
 	public static void main(String[] args) {
-
-		System.out.println("Una pruebica pa no liarnos");
 		
-		System.out.println("ESTO DEBERÍA DAR ERROR");
+		System.out.println("Prueba 2");
 		
 	}
 
